d18bc71e6082833c93b486edddd76fe840c826c9
JackYanx <1085473804@qq.com>
2018-04-26 00:23:42 +0800

    ui

--- app/src/main/java/com/zeromirai/classassistant/authentication/AuthenticationActivity.java
+++ app/src/main/java/com/zeromirai/classassistant/authentication/AuthenticationActivity.java
@@ -6,6 +6,7 @@ import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.TextView;
 
 import com.zeromirai.classassistant.R;
@@ -29,6 +30,7 @@ public class AuthenticationActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_authentication);
         setView();
         setListeners();
--- app/src/main/java/com/zeromirai/classassistant/authentication/fragment/LoginFragment.java
+++ app/src/main/java/com/zeromirai/classassistant/authentication/fragment/LoginFragment.java
@@ -2,11 +2,15 @@ package com.zeromirai.classassistant.authentication.fragment;
 
 
 import android.app.Activity;
-import android.os.Bundle;
 import android.app.Fragment;
+import android.content.Context;
+import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
 import com.zeromirai.classassistant.R;
 
@@ -16,6 +20,12 @@ import com.zeromirai.classassistant.R;
 public class LoginFragment extends Fragment {
 
     public Activity parentActivity;
+
+    private View view;
+
+    private EditText editText_userName;
+    private EditText editText_password;
+    private LinearLayout linearLayout_btn_login;
 
     public LoginFragment() {
         // Required empty public constructor
@@ -28,10 +38,56 @@ public class LoginFragment extends Fragment {
     }
 
     @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        this.parentActivity = (Activity) context;
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false);
+
+        view = inflater.inflate(R.layout.fragment_login, container, false);
+        setView();
+        setListeners();
+        return view;
+    }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        //view = parentActivity.getFragmentManager().findFragmentById(R.id.frameLayout_loginregfragment).getView();
+
+    }
+
+    private void setView(){
+        editText_userName = (EditText) view.findViewById(R.id.editText_userName);
+        linearLayout_btn_login = (LinearLayout) view.findViewById(R.id.linearLayout_btn_login);
+    }
+
+    private void setListeners(){
+
+        linearLayout_btn_login.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()){
+                    case MotionEvent.ACTION_DOWN:{
+                        linearLayout_btn_login.setBackgroundDrawable(getResources().getDrawable(R.drawable.shape_button_login_pressed));
+                        break;
+                    }
+                    case MotionEvent.ACTION_UP:{
+                        linearLayout_btn_login.setBackgroundDrawable(getResources().getDrawable(R.drawable.shape_button_login));
+                        break;
+                    }
+                    default:{ }
+                }
+                /*在onTouchEvent的Motion.ACTION_DOWN分支如果返回的是false，那么onTouch事件将不会往下面传递下去，如果返回true，那么onTouch事件将会传递下去，从而可以响应Motion.ACTION_UP分支。*/
+                return true;
+            }
+        });
+
+
     }
 
 }
