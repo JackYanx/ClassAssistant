a56a1f3cd355444ba75d68a8603991105b0f8b1b
h1astro <1065354834@qq.com>
2018-04-26 11:10:03 +0800

    update schedule
    add button in login_fragment goto scheduleactivity

--- app/src/main/java/com/zeromirai/classassistant/authentication/fragment/LoginFragment.java
+++ app/src/main/java/com/zeromirai/classassistant/authentication/fragment/LoginFragment.java
@@ -1,30 +1,110 @@
 package com.zeromirai.classassistant.authentication.fragment;
 
 
+import android.app.Activity;
+import android.app.Fragment;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
 import com.zeromirai.classassistant.R;
+import com.zeromirai.classassistant.schedule.ScheduleActivity;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class LoginFragment extends Fragment {
 
+    public Activity parentActivity;
+
+    private View view;
+
+    private EditText editText_userName;
+    private EditText editText_password;
+    private LinearLayout linearLayout_btn_login;
+
+
+    private Button button1;
+
 
     public LoginFragment() {
         // Required empty public constructor
     }
 
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        this.parentActivity = activity;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        this.parentActivity = (Activity) context;
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false);
+
+        view = inflater.inflate(R.layout.fragment_login, container, false);
+        setView();
+        setListeners();
+
+        button1=(Button)view.findViewById(R.id.clickTimetable);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getActivity(), ScheduleActivity.class));
+
+            }
+        });
+
+        return view;
+    }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        //view = parentActivity.getFragmentManager().findFragmentById(R.id.frameLayout_loginregfragment).getView();
+
+    }
+
+    private void setView(){
+        editText_userName = (EditText) view.findViewById(R.id.editText_userName);
+        linearLayout_btn_login = (LinearLayout) view.findViewById(R.id.linearLayout_btn_login);
+    }
+
+    private void setListeners(){
+
+        linearLayout_btn_login.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()){
+                    case MotionEvent.ACTION_DOWN:{
+                        linearLayout_btn_login.setBackgroundDrawable(getResources().getDrawable(R.drawable.shape_button_login_pressed));
+                        break;
+                    }
+                    case MotionEvent.ACTION_UP:{
+                        linearLayout_btn_login.setBackgroundDrawable(getResources().getDrawable(R.drawable.shape_button_login));
+                        break;
+                    }
+                    default:{ }
+                }
+                /*在onTouchEvent的Motion.ACTION_DOWN分支如果返回的是false，那么onTouch事件将不会往下面传递下去，如果返回true，那么onTouch事件将会传递下去，从而可以响应Motion.ACTION_UP分支。*/
+                return true;
+            }
+        });
+
+
     }
 
 }
--- app/src/main/java/com/zeromirai/classassistant/authentication/fragment/RegisterFragment.java
+++ app/src/main/java/com/zeromirai/classassistant/authentication/fragment/RegisterFragment.java
@@ -1,11 +1,16 @@
 package com.zeromirai.classassistant.authentication.fragment;
 
 
+import android.app.Activity;
+import android.app.Fragment;
+import android.content.Context;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
 import com.zeromirai.classassistant.R;
 
@@ -14,17 +19,78 @@ import com.zeromirai.classassistant.R;
  */
 public class RegisterFragment extends Fragment {
 
+    public static final String TAG = RegisterFragment.class.getName();
+
+    public Activity parentActivity;
+
+    private View view;
+
+    private EditText editText_userName;
+    private EditText editText_password;
+    private EditText editText_phoneNumber;
+    private EditText editText_emailAddress;
+    private LinearLayout linearLayout_btn_register;
 
     public RegisterFragment() {
         // Required empty public constructor
     }
 
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        this.parentActivity = activity;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        this.parentActivity = (Activity) context;
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false);
+        view = inflater.inflate(R.layout.fragment_register, container, false);
+        setView();
+        setListeners();
+        return view;
+    }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        //view = parentActivity.getFragmentManager().findFragmentById(R.id.frameLayout_loginregfragment).getView();
+
+    }
+
+    private void setView(){
+        editText_userName = (EditText) view.findViewById(R.id.editText_userName);
+        linearLayout_btn_register = (LinearLayout) view.findViewById(R.id.linearLayout_btn_register);
+    }
+
+    private void setListeners(){
+
+        linearLayout_btn_register.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()){
+                    case MotionEvent.ACTION_DOWN:{
+                        linearLayout_btn_register.setBackgroundDrawable(getResources().getDrawable(R.drawable.shape_button_register_pressed));
+                        break;
+                    }
+                    case MotionEvent.ACTION_UP:{
+                        linearLayout_btn_register.setBackgroundDrawable(getResources().getDrawable(R.drawable.shape_button_register));
+                        break;
+                    }
+                    default:{ }
+                }
+                /*在onTouchEvent的Motion.ACTION_DOWN分支如果返回的是false，那么onTouch事件将不会往下面传递下去，如果返回true，那么onTouch事件将会传递下去，从而可以响应Motion.ACTION_UP分支。*/
+                return true;
+            }
+        });
+
+
     }
 
 }
